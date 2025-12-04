ad772f91bf8160e2dc9c4cc3baddbdf6af522a1a
heshamalmosawi <hishamalmosawii@gmail.com>
2025-12-04 21:57:19 +0300

    test: specify application class in SpringBootTest for media and product services

--- backend/product-service/src/test/java/com/sayedhesham/ecommerce/EcommerceApplicationTests.java
+++ backend/product-service/src/test/java/com/sayedhesham/ecommerce/EcommerceApplicationTests.java
@@ -2,8 +2,9 @@ package com.sayedhesham.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.sayedhesham.productservice.ProductServiceApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = ProductServiceApplication.class)
 class EcommerceApplicationTests {
 
 	@Test
