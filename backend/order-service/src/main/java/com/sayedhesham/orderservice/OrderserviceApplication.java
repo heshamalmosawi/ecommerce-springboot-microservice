d28d5c78920e16d2f0c22689517a538282c99ed1
heshamalmosawi <hishamalmosawii@gmail.com>
2026-01-06 20:22:22 +0300

    Initialized order microservice

---
+++ backend/order-service/src/main/java/com/sayedhesham/orderservice/OrderserviceApplication.java
@@ -0,0 +1,33 @@
+package com.sayedhesham.orderservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.discovery.EurekaClient;
+
+@SpringBootApplication
+@RestController
+public class OrderserviceApplication {
+
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderserviceApplication.class, args);
+	}
+
+    @RequestMapping("/greeting")
+    public String greeting() {
+        return String.format("Hello from %s!", eurekaClient.getApplication(appName).getName());
+    }
+
+}
