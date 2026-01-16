4d4dfa69284a362624b9ca9171620350c5f1e42e
heshamalmosawi <hishamalmosawii@gmail.com>
2026-01-16 13:25:18 +0300

    feat: implement user-specific order retrieval with pagination and sorting

--- backend/order-service/src/main/java/com/sayedhesham/orderservice/service/Utils.java
+++ backend/order-service/src/main/java/com/sayedhesham/orderservice/service/Utils.java
@@ -4,7 +4,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
-class Utils {
+public class Utils {
 
     public static String getCurrentUserId() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
