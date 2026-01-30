841087e7014f79af777b93ccd2acd800dcbac153
heshamalmosawi <hishamalmosawii@gmail.com>
2026-01-30 21:00:10 +0300

    fix: update JWT secret handling to use application properties

--- backend/product-service/src/main/java/com/sayedhesham/productservice/service/security/JwtService.java
+++ backend/product-service/src/main/java/com/sayedhesham/productservice/service/security/JwtService.java
@@ -3,6 +3,7 @@ package com.sayedhesham.productservice.service.security;
 import java.security.Key;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.sayedhesham.productservice.model.User;
@@ -16,11 +17,13 @@ import io.jsonwebtoken.security.Keys;
 @Service
 public class JwtService {
 
-    // usually from .env but hardcoded since this is a playground project :)
-    private static final String SECRET = "a_very_long_and_not_secretive_secret_key_12345678901234567890123456789012";
-    private static final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+    private final Key key;
 
-    public static String generateToken(User user) {
+    public JwtService(@Value("${jwt.secret}") String secret) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateToken(User user) {
         return Jwts.builder()
                 .setSubject(user.getEmail())
                 .claim("role", user.getRole())
